fbb79fab2451f2bfdef4413bc89ed34a4e8b8d19
Ahmed HM <hm.Ahmed@fikrasystems.com>
2021-05-03 17:19:57 +0100

    Fix issues

---
+++ GreenFlux.TestConsole/CalculService.cs
@@ -0,0 +1,91 @@
+﻿using GreenFlux.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenFlux.TestConsole
+{
+    public class ConnectorRemovalSuggestionService
+    {
+        private readonly IReadOnlyList<ConnectorRemovalSuggestion> _allConnectorSuggestions;
+        private readonly Stack<ConnectorRemovalSuggestion> _usedConnectors;
+        private readonly IList<IList<ConnectorRemovalSuggestion>> _suggestions;
+
+        private ConnectorRemovalSuggestionService(IReadOnlyList<ConnectorRemovalSuggestion> allConnectorSuggestions)
+        {
+            _allConnectorSuggestions = allConnectorSuggestions;
+            _usedConnectors = new Stack<ConnectorRemovalSuggestion>();
+            _suggestions = new List<IList<ConnectorRemovalSuggestion>>();
+        }
+
+        public static IList<IList<ConnectorRemovalSuggestion>> SuggestConnectors(List<Connector> connectorsIn, float excessiveCurrent)
+        {
+
+            var connectors = connectorsIn
+                .Select(sc => new ConnectorRemovalSuggestion(sc.ChargeStationId, sc.CK_Id, sc.MaxCurrent))
+                .OrderByDescending(crs => crs.ConnectorCurrent)
+                .ToList();
+
+            var suggestionEngine = new ConnectorRemovalSuggestionService(connectors);
+
+            suggestionEngine.BuildSuggestions(excessiveCurrent);
+
+            var suggestionGroup = suggestionEngine._suggestions
+                .GroupBy(s => s.Count)
+                .OrderBy(g => g.Key)
+                .FirstOrDefault();
+
+            return suggestionGroup == null
+                ? Array.Empty<IList<ConnectorRemovalSuggestion>>()
+                : suggestionGroup.ToList() as IList<IList<ConnectorRemovalSuggestion>>;
+        }
+
+        private void BuildSuggestions(float current, int index = 0)
+        {
+            if (current == 0)
+            {
+                _suggestions.Add(new List<ConnectorRemovalSuggestion>(_usedConnectors));
+                return;
+            }
+
+            if (index >= _allConnectorSuggestions.Count || HasSuggestionBetterThanCurrent())
+            {
+                return;
+            }
+
+            var connector = _allConnectorSuggestions[index];
+            if (connector.ConnectorCurrent <= current)
+            {
+                _usedConnectors.Push(connector);
+                BuildSuggestions(current - connector.ConnectorCurrent, index + 1);
+                _usedConnectors.Pop();
+            }
+
+            BuildSuggestions(current, index + 1);
+        }
+
+        private bool HasSuggestionBetterThanCurrent()
+        {
+            return _suggestions.Count > 0 && _suggestions[0].Count < _usedConnectors.Count;
+        }
+    }
+
+
+
+    public struct ConnectorRemovalSuggestion
+    {
+        public long StationId;
+        public byte ConnectorId;
+        public float ConnectorCurrent;
+
+        public ConnectorRemovalSuggestion(long stationId, byte connectorId, float connectorCurrent)
+        {
+            StationId = stationId;
+            ConnectorId = connectorId;
+            ConnectorCurrent = connectorCurrent;
+        }
+    }
+
+
+}
---
+++ GreenFlux.TestConsole/Program.cs
@@ -0,0 +1,115 @@
+﻿using GreenFlux.Model;
+using GreenFlux.Service.Tools;
+using System;
+using System.Collections.Generic;
+
+namespace GreenFlux.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+            float target = 9;
+
+            var connectorsList = new List<Connector>();
+            connectorsList.Add(new Connector() { MaxCurrent = 6, ChargeStationId = 1, CK_Id = 1 });
+            connectorsList.Add(new Connector() { MaxCurrent = 5, ChargeStationId = 2, CK_Id = 2 });
+            connectorsList.Add(new Connector() { MaxCurrent = 4, ChargeStationId = 3, CK_Id = 3 });
+            connectorsList.Add(new Connector() { MaxCurrent = 3, ChargeStationId = 4, CK_Id = 4 });
+            connectorsList.Add(new Connector() { MaxCurrent = 3, ChargeStationId = 7, CK_Id = 7 });
+          //  connectorsList.Add(new Connector() { MaxCurrent = 3, ChargeStationId = 5, CK_Id = 5 });
+          //  connectorsList.Add(new Connector() { MaxCurrent = 1, ChargeStationId = 6, CK_Id = 6 });
+
+            //connectorsList.Add(new Connector() { MaxCurrent = 33, ChargeStationId = 1, CK_Id = 1 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 20, ChargeStationId = 2, CK_Id = 2 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 19, ChargeStationId = 3, CK_Id = 3 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 17, ChargeStationId = 4, CK_Id = 4 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 16, ChargeStationId = 4, CK_Id = 4 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 10, ChargeStationId = 5, CK_Id = 5 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 9, ChargeStationId = 6, CK_Id = 6 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 3, ChargeStationId = 7, CK_Id = 7 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 2, ChargeStationId = 8, CK_Id = 8 });
+            //connectorsList.Add(new Connector() { MaxCurrent = 17, ChargeStationId = 9, CK_Id = 9 });
+
+
+
+
+
+            ///////
+
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+
+            var suggestions2 = ConnectorRemovalSuggestionService.SuggestConnectors(connectorsList, target);
+
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+
+            Console.WriteLine("Suggestions count : " + suggestions2.Count);
+            Console.WriteLine("Time : " + elapsedMs2);
+
+            foreach (var item in suggestions2)
+            {
+                Console.WriteLine("Suggestion Start : ***********");
+                foreach (var element in item)
+                {
+                    Console.WriteLine($"ID : {element.ConnectorId} chargeStationID : {element.StationId} MaxCurrent : {element.ConnectorCurrent}");
+                }
+                Console.WriteLine("Suggestion End  **************");
+            }
+
+
+
+
+
+            Console.WriteLine("\nThe Other Solution  //////////////////////////**************");
+            Console.WriteLine("**************\n");
+
+
+            var suggester = new Suggester();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var suggestions = suggester.GetSuggestions(connectorsList, target);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("My Suggestions count : " + suggestions.Count);
+            Console.WriteLine("My Time : " + elapsedMs);
+
+            foreach (var item in suggestions)
+            {
+                Console.WriteLine("Suggestion Start : ***********");
+                if (item is SingleElementSuggestion)
+                {
+                    var sinSuggestion = (SingleElementSuggestion)item;
+                    Console.WriteLine($"ID : {sinSuggestion.Element.CK_Id} chargeStationID : {sinSuggestion.Element.ChargeStationId} MaxCurrent : {sinSuggestion.Element.MaxCurrent}");
+                }
+                else
+                {
+                    var muSuggestion = (MultipleElementSuggestion)item;
+                    foreach (var element in muSuggestion.Elements)
+                    {
+                        Console.WriteLine($"ID : {element.CK_Id} chargeStationID : {element.ChargeStationId} MaxCurrent : {element.MaxCurrent}");
+                    }
+                }
+                Console.WriteLine("Suggestion End  **************");
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
